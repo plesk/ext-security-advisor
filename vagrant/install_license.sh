21e86ec76e8a259812b3f9a4da0adaf32d9f1d77
Eugene Kazakov <ekazakov@plesk.com>
2016-05-25 19:32:33 +0600

    Install Web Host edition license

--- vagrant/install_license.sh
+++ vagrant/install_license.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ### Copyright 1999-2016. Parallels IP Holdings GmbH. All Rights Reserved.
 
-plesk bin license --install /vagrant/vagrant/plesk_unified.xml.zip
+plesk bin license --install /vagrant/vagrant/plesk_12_web_host_90_days_developer_license.xml
