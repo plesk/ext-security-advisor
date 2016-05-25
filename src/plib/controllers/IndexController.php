bfcca7f6d4643add8dd16359524c6c41f647660d
Vladimir Liktanskiy <vlikhtanskiy@plesk.com>
2016-05-25 13:24:20 +0600

    Add tabs

--- src/plib/controllers/IndexController.php
+++ src/plib/controllers/IndexController.php
@@ -2,7 +2,41 @@
 
 class IndexController extends pm_Controller_Action
 {
+    public function init()
+    {
+        parent::init();
+
+        $this->view->pageTitle = $this->lmsg('pageTitle');
+        $this->view->tabs = [
+            [
+                'title' => $this->lmsg('tabs.domains'),
+                'action' => 'domain-list',
+            ],
+            [
+                'title' => $this->lmsg('tabs.wordpress'),
+                'action' => 'wordpress-list',
+            ],
+            [
+                'title' => $this->lmsg('tabs.settings'),
+                'action' => 'settings',
+            ],
+        ];
+    }
+
     public function indexAction()
+    {
+        $this->_forward('domain-list');
+    }
+
+    public function domainListAction()
+    {
+    }
+
+    public function wordpressListAction()
+    {
+    }
+
+    public function settingsAction()
     {
     }
 }
---
+++ src/plib/resources/locales/en-US.php
@@ -0,0 +1,8 @@
+<?php
+
+$messages = [
+    'pageTitle' => 'Security Wizard',
+    'tabs.domains' => 'Domains',
+    'tabs.wordpress' => 'Word Press',
+    'tabs.settings' => 'Settings',
+];
