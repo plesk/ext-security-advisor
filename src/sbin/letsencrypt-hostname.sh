ea295e25272d2fa220688b611415b5f1631c001c
Eugene Kazakov <ekazakov@plesk.com>
2016-05-25 08:47:07 -0700

    Symlink sbin directory

---
+++ src/sbin/letsencrypt-hostname.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+### Copyright 1999-2015. Parallels IP Holdings GmbH. All Rights Reserved.
+### Secure plesk clean installation with hostname certificate by Let's Encrypt
+
+export PYTHONWARNINGS="ignore:Non-standard path"
+LE_HOME=${LE_HOME:-"/usr/local/psa/var/modules/letsencrypt"}
+HOSTNAME=$(hostname)
+
+# Use staging server for testing
+# --server https://acme-staging.api.letsencrypt.org/directory
+# --server http://letsencrypt.pp.plesk.ru/directory
+
+"${LE_HOME}/venv/bin/letsencrypt" \
+    --renew-by-default \
+    --no-redirect \
+    --agree-tos \
+    --text \
+    --config-dir "${LE_HOME}/root/etc" \
+    --work-dir "${LE_HOME}/root/lib" \
+    --logs-dir "${LE_HOME}/root/logs" \
+    --webroot \
+    --webroot-path "/var/www/vhosts/default/htdocs/" \
+    -d "${HOSTNAME}" \
+    --register-unsafely-without-email \
+    certonly
+
+CERT_PATH="${LE_HOME}/root/etc/live/${HOSTNAME}"
+TMP_PATH=$(mktemp "${CERT_PATH}/plesk.XXXXX")
+cat "${CERT_PATH}/privkey.pem" <(echo) \
+    "${CERT_PATH}/cert.pem" <(echo) \
+    "${CERT_PATH}/chain.pem" > "${TMP_PATH}"
+echo "Let's Encrypt certificate for Plesk was created: ${TMP_PATH}"
+/usr/local/psa/admin/bin/certmng --setup-cp-certificate --certificate="${TMP_PATH}"
+echo "Certificate installation was finished successfully"
--- vagrant/mount_ext.sh
+++ vagrant/mount_ext.sh
@@ -7,16 +7,24 @@ SRC=/vagrant/src
 
 create_symlink()
 {
-	TARGET=$1
-	SOURCE=$2
-	if [ ! -L $TARGET ]; then
-		[ -d $TARGET ] && mv $TARGET $TARGET.old
-		ln -s $SOURCE $TARGET
-	fi
+    TARGET=$1
+    SOURCE=$2
+    if [ ! -L $TARGET ]; then
+        [ -d $TARGET ] && mv $TARGET $TARGET.old
+        ln -s $SOURCE $TARGET
+    fi
 }
 
 create_symlink $BASE/admin/htdocs/modules/$EXT $SRC/htdocs
 create_symlink $BASE/admin/plib/modules/$EXT $SRC/plib
+
+create_symlink $BASE/admin/sbin/modules/$EXT $SRC/sbin
+[ -d $BASE/admin/bin/modules/$EXT ] || mkdir $BASE/admin/bin/modules/$EXT
+for FILE in $SRC/sbin/*; do
+    UTIL=$(basename $FILE)
+    ln -s ../../../sbin/mod_wrapper $BASE/admin/bin/modules/$EXT/$UTIL
+done
+
 [ -d $BASE/var/modules/$EXT ] || mkdir $BASE/var/modules/$EXT
 [ -f $BASE/admin/plib/modules/$EXT/meta.xml ] || cp $SRC/meta.xml $BASE/admin/plib/modules/$EXT/meta.xml
 plesk bin extension --register $EXT
