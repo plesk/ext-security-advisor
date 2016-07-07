a97ac51bc2457d6affb64ae00a73fb30662b8e3c
Eugene Kazakov <ekazakov@plesk.com>
2016-07-07 17:18:43 +0600

    Move secure panel to helper

--- src/plib/library/Helper/PanelCertificate.php
+++ src/plib/library/Helper/PanelCertificate.php
@@ -20,11 +20,49 @@ class Modules_SecurityAdvisor_Helper_PanelCertificate
 
     public static function verifyCertificate($certData)
     {
-        $caInfo = [pm_Context::getPlibDir() . 'resources/ca'];
+        $caInfo = [
+            pm_Context::getPlibDir() . 'resources/ca',
+            pm_Context::getPlibDir() . 'resources/ca/cacert.pem',
+            pm_Context::getPlibDir() . 'resources/ca/letsencrypt-root.pem', // for testing purpose
+        ];
         $x509 = openssl_x509_read($certData);
         if (empty($x509)) {
-                return false;
+            return false;
         }
         return (bool)openssl_x509_checkpurpose($x509, X509_PURPOSE_SSL_SERVER, $caInfo);
+    }
+
+    public static function securePanel($hostname)
+    {
+        if (static::isDomainRegisteredInPlesk($hostname)) {
+            Modules_SecurityAdvisor_Letsencrypt::run($hostname, true);
+        } else {
+            $res = pm_ApiCli::callSbin('letsencrypt-hostname.sh', [$hostname]);
+            if ($res['code']) {
+                throw new pm_Exception($res['stdout'] . $res['stderr']);
+            }
+        }
+        pm_Settings::set('secure-panel-hostname', $hostname);
+    }
+
+    private function isDomainRegisteredInPlesk($domain)
+    {
+        $request = <<<APICALL
+        <site>
+            <get>
+                <filter>
+                    <name>{$domain}</name>
+                </filter>
+                <dataset>
+                    <gen_info/>
+                </dataset>
+            </get>
+        </site>
+APICALL;
+        $response = pm_ApiRpc::getService()->call($request);
+        if ($response->site->get->result->status == 'error' && '1013' == $response->site->get->result->errcode) {
+            return false;
+        }
+        return true;
     }
 }
--- src/plib/library/View/Form/SecurePanel.php
+++ src/plib/library/View/Form/SecurePanel.php
@@ -32,15 +32,7 @@ class Modules_SecurityAdvisor_View_Form_SecurePanel extends pm_Form_Simple
     public function process()
     {
         $hostname = $this->securePanelHostname->getValue();
-        if ($this->_isDomainRegisteredInPlesk($hostname)) {
-            Modules_SecurityAdvisor_Letsencrypt::run($hostname, true);
-        } else {
-            $res = pm_ApiCli::callSbin('letsencrypt-hostname.sh', [$hostname]);
-            if ($res['code']) {
-                throw new pm_Exception($res['stdout'] . $res['stderr']);
-            }
-        }
-        pm_Settings::set('secure-panel-hostname', $hostname);
+        Modules_SecurityAdvisor_Helper_PanelCertificate::securePanel($hostname);
     }
 
     private function _getHostname()
@@ -59,26 +51,5 @@ class Modules_SecurityAdvisor_View_Form_SecurePanel extends pm_Form_Simple
             return $hostname;
         }
         return Modules_SecurityAdvisor_Helper_Hostname::getServerHostname();
-    }
-
-    private function _isDomainRegisteredInPlesk($domain)
-    {
-        $request = <<<APICALL
-        <site>
-            <get>
-                <filter>
-                    <name>{$domain}</name>
-                </filter>
-                <dataset>
-                    <gen_info/>
-                </dataset>
-            </get>
-        </site>
-APICALL;
-        $response = pm_ApiRpc::getService()->call($request);
-        if ($response->site->get->result->status == 'error' && '1013' == $response->site->get->result->errcode) {
-            return false;
-        }
-        return true;
     }
 }
