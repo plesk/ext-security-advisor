fce4626f7e684c59c03db64d313912f21c2bbef7
atikhonov <alex.vl.tihon@gmail.com>
2017-11-13 13:42:00 +0700

    BUGFIX EXTPLESK-347 FIX: Domain with assigned invalid certificate are not counted in badge

--- src/plib/library/Helper/Domain.php
+++ src/plib/library/Helper/Domain.php
@@ -52,4 +52,72 @@ class Domain
             return $pmDomain->getId();
         }, $pmDomains);
     }
+
+    /**
+     * Return count of domains without certificate or with invalid certificate
+     *
+     * @param int $webspaceId
+     * @return int
+     */
+    public static function countInsecure($webspaceId = 0)
+    {
+        $count = 0;
+
+        $client = \pm_Session::getClient();
+        $clientId = $client->getId();
+
+        $certDbTable = \Db_Table_Broker::get('Certificates');
+        $select = $certDbTable->select()
+            ->setIntegrityCheck(false)
+            ->from(
+                [
+                    'h' => 'hosting',
+                ]
+            )
+            ->joinLeft(
+                [
+                    'c' => 'certificates'
+                ],
+                'c.id = h.certificate_id'
+            );
+
+        if (!$client->isAdmin() && !$webspaceId) {
+            $select = $select
+                ->join(
+                    [
+                        'd' => 'domains'
+                    ],
+                    'd.id = h.dom_id'
+                )
+                ->where('d.cl_id = ? OR d.vendor_id = ?', $clientId, $clientId);
+        } elseif ($webspaceId) {
+            $select = $select
+                ->join(
+                    [
+                        'd' => 'domains'
+                    ],
+                    'd.id = h.dom_id'
+                )
+                ->where('d.id = ? OR d.webspace_id = ?', $webspaceId, $webspaceId);
+        }
+
+        $items = $certDbTable->fetchAll($select);
+
+        foreach ($items as $item) {
+            if (!intval($item->certificate_id)
+                || !($cert = urldecode($item->cert))
+                || !($ssl = openssl_x509_parse($cert))
+            ) {
+                $count++;
+                continue;
+            }
+
+            $certData = ($item->ca_cert ? urldecode($item->ca_cert) . "\n" : "") . $cert;
+            if (!\Modules_SecurityAdvisor_Helper_Ssl::verifyCertificate($certData)) {
+                $count++;
+            }
+        }
+
+        return $count;
+    }
 }
--- src/plib/library/Helper/Utils.php
+++ src/plib/library/Helper/Utils.php
@@ -3,30 +3,6 @@
 
 class Modules_SecurityAdvisor_Helper_Utils
 {
-    public static function countInsecureDomains($webspaceId = 0)
-    {
-        $client = \pm_Session::getClient();
-        $clientId = $client->getId();
-
-        if (!$client->isAdmin() && !$webspaceId) {
-            return \pm_Bootstrap::getDbAdapter()->fetchOne(
-                'SELECT COUNT(*) FROM hosting h JOIN domains d ON d.id = h.dom_id'
-                . ' WHERE (cl_id = ? OR vendor_id = ?) AND h.certificate_id = 0',
-                [$clientId, $clientId]
-            );
-        }
-
-        if ($webspaceId) {
-            return \pm_Bootstrap::getDbAdapter()->fetchOne(
-                'SELECT COUNT(*) FROM hosting h JOIN domains d ON d.id = h.dom_id'
-                . ' WHERE (id = ? OR webspace_id = ?) AND h.certificate_id = 0',
-                [$webspaceId, $webspaceId]
-            );
-        } else {
-            return \pm_Bootstrap::getDbAdapter()->fetchOne('SELECT COUNT(*) FROM hosting WHERE certificate_id = 0');
-        }
-    }
-
     public static function getOsVersion()
     {
         return method_exists('pm_ProductInfo', 'getOsVersion')
